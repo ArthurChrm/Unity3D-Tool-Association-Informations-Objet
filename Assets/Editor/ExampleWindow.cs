70c8a069a7328882f56d50b4da9eaaccde415bfe
Arthur <arthur.cheramy@gmail.com>
2019-02-10 11:06:03 -0500

    Création de la fenêtre du tool
    Les erreurs de séléction sont aussi gérées

---
+++ Assets/Editor/ExampleWindow.cs
@@ -0,0 +1,68 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+public class ExampleWindow : EditorWindow
+{
+
+
+    private string ERREUR_AUCUNE_SELECTION = "Séléctionnez un ou plusieurs éléments !";
+    private string ERREUR_PAS_ENVIRONNEMENT = "Votre séléction comporte un ou plusieurs éléments ne contenant pas de composant \"Environnement\"";
+    private string labelSelectionObjet = "";
+    private string labelErreurSelection = "";
+
+    [MenuItem("Window/Example")]
+    public static void ShowWindow()
+    {
+        GetWindow<ExampleWindow>("Example");
+    }
+    void OnGUI()
+    {
+        // Window code
+        GUILayout.Label(labelSelectionObjet);
+        GUILayout.Label(labelErreurSelection);
+    }
+    void Update()
+    {
+
+        checkSiSelectionVide();
+        checkSiSelectionValide();
+
+
+
+        // On va vérifier si un objet est séléctionné
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            Debug.Log(obj.GetComponent<Environnement>());
+            //Debug.Log(obj.ToString());
+        }
+    }
+
+    void checkSiSelectionVide()
+    {
+        // Va vérifier si l'utilisateur a séléctionné un élément
+        if (Selection.gameObjects.Length == 0)
+        {
+            labelSelectionObjet = ERREUR_AUCUNE_SELECTION;
+        }
+        else
+        {
+            labelSelectionObjet = null;
+        }
+    }
+
+    void checkSiSelectionValide()
+    {
+        // Va vérifier si la séléction ne comporte que des gameObjects ayant le type "Environnement"
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            if (obj.GetComponent<Environnement>() == null)
+            {
+                labelErreurSelection = ERREUR_PAS_ENVIRONNEMENT;
+                return;
+            }
+        }
+        labelErreurSelection = null;
+    }
+
+}
---
+++ Assets/Scripts/Environnement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environnement : MonoBehaviour
+{
+    public string nom;
+    public string description;
+    public float prix;
+    public float resistance;
+}
